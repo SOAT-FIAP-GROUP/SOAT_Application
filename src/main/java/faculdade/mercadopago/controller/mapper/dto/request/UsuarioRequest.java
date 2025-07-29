c60c460d2cbdeb4cb05199d9bfc82fbb83e009d6
Rogerio Inacio <rogerio.reaper@gmail.com>
2025-07-29 14:16:29 -0300

    refactor: create a public method to validate dto instead of validate on controller

--- src/main/java/faculdade/mercadopago/controller/mapper/dto/request/UsuarioRequest.java
+++ src/main/java/faculdade/mercadopago/controller/mapper/dto/request/UsuarioRequest.java
@@ -1,11 +1,13 @@
 package faculdade.mercadopago.controller.mapper.dto.request;
 
+import faculdade.mercadopago.exception.BusinessException;
+
 import java.util.Objects;
 
 public record UsuarioRequest(Boolean identificarUsuario, String nome, String cpf, String email) {
-    public UsuarioRequest {
+    public void validar() {
         if (Objects.equals(identificarUsuario, null)) {
-            throw new IllegalArgumentException("O campo 'identificarUsuario' é obrigatório.");
+            throw new BusinessException("O campo 'identificarUsuario' é obrigatório.");
         }
 
         if (Objects.equals(identificarUsuario, true)) {
@@ -17,35 +19,35 @@ public record UsuarioRequest(Boolean identificarUsuario, String nome, String cpf
 
     private void validarNome(String nome) {
         if (Objects.equals(nome, null) || nome.isBlank()) {
-            throw new IllegalArgumentException("O campo 'nome' é obrigatório quando 'identificarUsuario' for falso.");
+            throw new BusinessException("O campo 'nome' é obrigatório quando 'identificarUsuario' for falso.");
         }
 
         if (nome.length() < 3 || nome.length() > 50) {
-            throw new IllegalArgumentException("O campo 'nome' deve conter entre 3 e 50 caracteres.");
+            throw new BusinessException("O campo 'nome' deve conter entre 3 e 50 caracteres.");
         }
     }
 
     private void validarCpf(String cpf) {
         if (Objects.equals(cpf, null) || cpf.isBlank()) {
-            throw new IllegalArgumentException("O campo 'cpf' é obrigatório quando 'identificarUsuario' for verdadeiro.");
+            throw new BusinessException("O campo 'cpf' é obrigatório quando 'identificarUsuario' for verdadeiro.");
         }
 
         if (cpf.length() != 11) {
-            throw new IllegalArgumentException("O campo 'cpf' deve conter exatamente 11 caracteres.");
+            throw new BusinessException("O campo 'cpf' deve conter exatamente 11 caracteres.");
         }
 
         if (!cpf.matches("\\d+")) {
-            throw new IllegalArgumentException("O campo 'cpf' deve conter apenas números.");
+            throw new BusinessException("O campo 'cpf' deve conter apenas números.");
         }
     }
 
     private void validarEmail(String email) {
         if (Objects.equals(email, null) || email.isBlank()) {
-            throw new IllegalArgumentException("O campo 'email' é obrigatório.");
+            throw new BusinessException("O campo 'email' é obrigatório.");
         }
 
         if (!email.matches("^[\\w-.]+@[\\w-]+\\.[a-zA-Z]{2,}$")) {
-            throw new IllegalArgumentException("O campo 'email' deve ser um endereço de e-mail válido.");
+            throw new BusinessException("O campo 'email' deve ser um endereço de e-mail válido.");
         }
     }
 }
